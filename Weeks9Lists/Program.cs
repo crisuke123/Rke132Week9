8c39156a5bba550b72fb9882b1ce1c984a2d73ce
cris <crisuke123@gmail.com>
2023-01-07 19:48:13 +0200

    add week9 code

---
+++ Weeks9Lists/Program.cs
@@ -0,0 +1,61 @@
+﻿string folderPath = @"C:\Data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+
+List<string> myShoppingList = new List<string>();
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+   File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"file {fileName} has been greated.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+
+
+
+static List<string> GetItemsFromUser()
+{
+    List<string> userList = new List<string>();
+
+    while (true)
+    {
+        Console.WriteLine($"Add an item (add)/Exit(exit):");
+        string userChoice = Console.ReadLine();
+        if (userChoice == "add")
+        {
+            Console.WriteLine("Enter an item:");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem);
+        }
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+        }
+
+    }
+    return userList ;
+}
+
+static void ShowItemsFromList(List<string> SomeList)
+{
+    Console.Clear();
+
+    int listLength = SomeList.Count;
+    Console.WriteLine($"You have added {listLength} items to your shopping list.");
+
+    int i = 1;
+    foreach (string item in SomeList)
+    {
+        Console.WriteLine($"{i}. {item}");
+        i++;
+    }
+}
+
